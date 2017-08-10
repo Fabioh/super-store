71ba86cff74b710e236ff7ada6df64d849c880de
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2017-08-09 23:56:25 -0300

    import lodash and jquery library in productsService

--- src/app/providers/products.service.ts
+++ src/app/providers/products.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
 
+const porducts = require('./products.json');
+const BASEURL = window.location.href;
+
+import * as $ from 'jquery';
+import * as _ from 'lodash';
+
 @Injectable()
 export class ProductsService {
 
-  constructor() { }
+  constructor() {
+
+  }
 
 }
