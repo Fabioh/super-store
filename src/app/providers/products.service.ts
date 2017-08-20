defb51239eaa6f94eefa0a06c7d2c23654778376
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2017-08-20 14:59:02 -0300

    productsServices working

--- src/app/providers/products.service.ts
+++ src/app/providers/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
-const porducts = require('./products.json');
-const BASEURL = window.location.href;
+const products = require('./products.json');
+const BASEURL = window.location.href.substring(0, window.location.href.length - 1) ;
 
 import * as $ from 'jquery';
 import * as _ from 'lodash';
@@ -9,8 +9,20 @@ import * as _ from 'lodash';
 @Injectable()
 export class ProductsService {
 
-  constructor() {
+  constructor() { }
 
+  list() {
+    return new Promise((resolve, reject) => {
+      products.map((product, i) => {
+        // título no-formato-de-slug
+        product.titleSlug = _.kebabCase(product.name);
+        // a rota deste produto
+        product.route = '/products/' + product.titleSlug + '/' + product.id;
+        // a url deste produto
+        product.url = BASEURL + product.route;
+      });
+      resolve(products);
+    });
   }
 
 }
